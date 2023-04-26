cce68bd9d65c7bcffa3f579e2dcc7bbf1be2ffd6
DESKTOP-I4LDUPH\hi <ranger_b@naver.com>
2023-04-26 13:46:55 +0900

    놀이공원 test 001

---
+++ Project230426/src/Kopo40/Main.java
@@ -0,0 +1,20 @@
+package Kopo40;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner myInput = new Scanner(System.in);
+		int repeat;
+		Hello aa = new Hello();
+		do {
+			aa.input();
+			aa.calc();
+			aa.saveData();
+			System.out.print("계속 하시겠습니까? (0:YES, 1:NO) : ");
+			repeat = myInput.nextInt();
+		} while (repeat == 0);
+		aa.output();
+	}
+}
---
+++ Project230426/src/Kopo40/saveData.java
@@ -0,0 +1,9 @@
+package Kopo40;
+
+public class SData {
+	void saveData() {
+		orderList = new OrderListClass(ticketGrade,
+						totalPrice, ticketType, ticketCount);
+		data.add(orderList);
+	}
+}
